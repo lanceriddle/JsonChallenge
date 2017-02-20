cb48ec7e698943585c7c2397971218051472c310
Lance Riddle <riddle@xmission.com>
2017-02-19 22:57:30 -0800

    Replace tabs with spaces.

--- src/com/challenge/json/JsonMain.java
+++ src/com/challenge/json/JsonMain.java
@@ -11,58 +11,58 @@ import java.util.Scanner;
  */
 public class JsonMain {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		// JSON Input
-		String itemList = "{\"itemList\": {\"items\": ["
-						+ "{\"id\": \"item1\"},"
-						+ "{\"id\": \"item2\",\"label\": \"Item 2\"},"
-						+ "{\"id\": \"item3\"},"
-						+ "{\"id\": \"item4\"},"
-						+ "{\"id\": \"item5\"},"
-						+ "{\"id\": \"subItem1\","
-						+ "\"subItems\": ["
-						+ "{\"id\": \"subItem1Item1\",\"label\": \"SubItem 1\"},"
-						+ "{\"id\": \"subItem1Item2\",\"label\": \"SubItem 2\"},"
-						+ "{\"id\": \"subItem1Item3\",\"label\": \"SubItem 3\"}"
-						+ "]"
-						+ "},"
-						+ "{\"id\": \"item6\"},"
-						+ "{\"id\": \"item7\",\"label\": \"Item 7\"},"
-						+ "{\"id\": \"subItem2\","
-						+ "\"subItems\": {\"id\": \"item1\",\"label\": \"SubItem 2 item1\"}"
-						+ "}"
-						+ "]}}";
+        // JSON Input
+        String itemList = "{\"itemList\": {\"items\": ["
+                + "{\"id\": \"item1\"},"
+                + "{\"id\": \"item2\",\"label\": \"Item 2\"},"
+                + "{\"id\": \"item3\"},"
+                + "{\"id\": \"item4\"},"
+                + "{\"id\": \"item5\"},"
+                + "{\"id\": \"subItem1\","
+                + "\"subItems\": ["
+                + "{\"id\": \"subItem1Item1\",\"label\": \"SubItem 1\"},"
+                + "{\"id\": \"subItem1Item2\",\"label\": \"SubItem 2\"},"
+                + "{\"id\": \"subItem1Item3\",\"label\": \"SubItem 3\"}"
+                + "]"
+                + "},"
+                + "{\"id\": \"item6\"},"
+                + "{\"id\": \"item7\",\"label\": \"Item 7\"},"
+                + "{\"id\": \"subItem2\","
+                + "\"subItems\": {\"id\": \"item1\",\"label\": \"SubItem 2 item1\"}"
+                + "}"
+                + "]}}";
 
-		// Run a loop to prompt the user for names to search for,
-		// until they enter "exit".
-		Scanner scanner = new Scanner(System.in);
-		String element;
-		try {
-			while (true) {
-				System.out.print("Enter an element name to search for (\"exit\" to close): ");
-				element = scanner.nextLine();
+        // Run a loop to prompt the user for names to search for,
+        // until they enter "exit".
+        Scanner scanner = new Scanner(System.in);
+        String element;
+        try {
+            while (true) {
+                System.out.print("Enter an element name to search for (\"exit\" to close): ");
+                element = scanner.nextLine();
 
-				if (element != null) {
-					// Trim any possible quotes.
-					element = element.replaceAll("^\"|\"$", "");
+                if (element != null) {
+                    // Trim any possible quotes.
+                    element = element.replaceAll("^\"|\"$", "");
 
-					// Check whether to exit.
-					if (element.equalsIgnoreCase("exit"))
-						break;
+                    // Check whether to exit.
+                    if (element.equalsIgnoreCase("exit"))
+                        break;
 
-					// Use the JsonParser to find the element's path.
-					System.out.println(String.format("\"%s\": %s\n",
-							element,
-							JsonParser.getFullPath(itemList, element)));
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			if (scanner != null)
-				scanner.close();
-		}
-		System.out.println("\nGoodbye!");
-	}
+                    // Use the JsonParser to find the element's path.
+                    System.out.println(String.format("\"%s\": %s\n",
+                            element,
+                            JsonParser.getFullPath(itemList, element)));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (scanner != null)
+                scanner.close();
+        }
+        System.out.println("\nGoodbye!");
+    }
 }
--- src/com/challenge/json/JsonParser.java
+++ src/com/challenge/json/JsonParser.java
@@ -19,93 +19,93 @@ import javax.json.JsonValue;
  */
 public class JsonParser {
 
-	/**
-	 * Return the full path of a specified element in the JSON object.
-	 * 
-	 * @param jsonString The full JSON object notation string.
-	 * @param element The element name to search for.
-	 * 
-	 * @return The full path of the element if it was found, an error message otherwise.
-	 */
-	public static String getFullPath(String jsonString, String element) {
-		// Create a JsonObject from the full notation.
-		JsonReader reader = Json.createReader(new StringReader(jsonString));
-		JsonObject obj = reader.readObject();
-		reader.close();
+    /**
+     * Return the full path of a specified element in the JSON object.
+     * 
+     * @param jsonString The full JSON object notation string.
+     * @param element The element name to search for.
+     * 
+     * @return The full path of the element if it was found, an error message otherwise.
+     */
+    public static String getFullPath(String jsonString, String element) {
+        // Create a JsonObject from the full notation.
+        JsonReader reader = Json.createReader(new StringReader(jsonString));
+        JsonObject obj = reader.readObject();
+        reader.close();
 
-		// Recursively search/build the path.
-		String path = findElement(obj, null, element);
-		if (path == null)
-			return "Element was not found.";
-		else
-			return "\\" + path;
-	}
+        // Recursively search/build the path.
+        String path = findElement(obj, null, element);
+        if (path == null)
+            return "Element was not found.";
+        else
+            return "\\" + path;
+    }
 
-	/**
-	 * Recursively search the JSON object's values.
-	 * 
-	 * @param jsonValue The current JsonValue being examined.
-	 * @param key The key for the current JSON element.
-	 * @param elementToFind The element name being searched for.
-	 * 
-	 * @return The result of checking the current node.
-	 */
-	private static String findElement(JsonValue jsonValue, String key, String elementToFind) {
-		if (jsonValue == null || elementToFind == null) {
-			return null;
-		}
+    /**
+     * Recursively search the JSON object's values.
+     * 
+     * @param jsonValue The current JsonValue being examined.
+     * @param key The key for the current JSON element.
+     * @param elementToFind The element name being searched for.
+     * 
+     * @return The result of checking the current node.
+     */
+    private static String findElement(JsonValue jsonValue, String key, String elementToFind) {
+        if (jsonValue == null || elementToFind == null) {
+            return null;
+        }
 
-		// Handle the cases for each JsonValue type.
-		switch(jsonValue.getValueType()) {
-			case OBJECT:
-				// Loop through all the elements in the Object recursively.
-				JsonObject object = (JsonObject) jsonValue;
-				for (String name : object.keySet()) {
-					String path = findElement(object.get(name), name, elementToFind);
-					
-					// If 'path' isn't null, it's because the element was found, so prepend and return.
-					if (path != null) {
-						if (key != null)
-							return key + "\\" + path;
-						else
-							return path;
-					}
-				}
-				break;
-			case ARRAY:
-				// Loop through all the elements in the Array recursively.
-				JsonArray array = (JsonArray) jsonValue;
-				int idx = 0;
-				for (JsonValue value : array) {
-					String path = findElement(value, null, elementToFind);
-					
-					// If 'path' isn't null, it's because the element was found, so prepend and return.
-					if (path != null)
-						return String.format("%s[%d]\\%s", key, idx, path);
-					idx++;
-				}
-				break;
-			case STRING:
-				// We have reached an end node, so compare the value and return.
-				JsonString str = (JsonString) jsonValue;
-				if (str.getString().equals(elementToFind)) {
-					return key;
-				}
-				break;
-			case NUMBER:
-				// We have reached an end node, so compare the value and return.
-				JsonNumber number = (JsonNumber) jsonValue;
-				if (number.toString().equals(elementToFind)) {
-					return key;
-				}
-				break;
-			case TRUE:
-			case FALSE:
-			case NULL:
-			default:
-				break;
-		}
+        // Handle the cases for each JsonValue type.
+        switch(jsonValue.getValueType()) {
+        case OBJECT:
+            // Loop through all the elements in the Object recursively.
+            JsonObject object = (JsonObject) jsonValue;
+            for (String name : object.keySet()) {
+                String path = findElement(object.get(name), name, elementToFind);
 
-		return null;
-	}
+                // If 'path' isn't null, it's because the element was found, so prepend and return.
+                if (path != null) {
+                    if (key != null)
+                        return key + "\\" + path;
+                    else
+                        return path;
+                }
+            }
+            break;
+        case ARRAY:
+            // Loop through all the elements in the Array recursively.
+            JsonArray array = (JsonArray) jsonValue;
+            int idx = 0;
+            for (JsonValue value : array) {
+                String path = findElement(value, null, elementToFind);
+
+                // If 'path' isn't null, it's because the element was found, so prepend and return.
+                if (path != null)
+                    return String.format("%s[%d]\\%s", key, idx, path);
+                idx++;
+            }
+            break;
+        case STRING:
+            // We have reached an end node, so compare the value and return.
+            JsonString str = (JsonString) jsonValue;
+            if (str.getString().equals(elementToFind)) {
+                return key;
+            }
+            break;
+        case NUMBER:
+            // We have reached an end node, so compare the value and return.
+            JsonNumber number = (JsonNumber) jsonValue;
+            if (number.toString().equals(elementToFind)) {
+                return key;
+            }
+            break;
+        case TRUE:
+        case FALSE:
+        case NULL:
+        default:
+            break;
+        }
+
+        return null;
+    }
 }
